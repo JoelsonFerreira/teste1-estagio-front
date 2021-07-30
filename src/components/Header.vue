1bf4e191faa86daf1ed5ebb1de23fcc921eab0cd
Joelson Ferreira <joelsonidalgo@gmail.com>
2021-07-30 11:56:08 -0300

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img src="@/assets/logo_ic.svg" alt="Logo Intuitive Care">
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+header {
+    padding: 20px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    position: sticky;
+    top: 0;
+    background: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
