9205975277039d46b76c0bc058eefc0516991754
Joelson Ferreira <joelsonidalgo@gmail.com>
2021-07-30 11:54:29 -0300

    first page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <main>
+    <DataContainer />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import DataContainer from './components/DataContainer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    DataContainer
   }
-}
+};
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+
+  background: #F1F1F1;
+  height: calc(100vh - 87px);
+  width: 100%;
+  z-index: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  padding: 50px;
 }
 </style>
